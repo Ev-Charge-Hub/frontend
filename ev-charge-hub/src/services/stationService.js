d8c8b5c3b590430281bee9fa7c6328c0c13df04c
Jiratchaya Thongsuthum <mjrchy@Jiratchayas-MacBook-Air.local>
2025-02-06 21:10:06 +0700

    use mock station

--- ev-charge-hub/src/services/stationService.js
+++ ev-charge-hub/src/services/stationService.js
@@ -20,47 +20,9 @@ export const stationService = {
     }
 
     , async getStationById(stationId) {
-        const station = {
-            "station_id": "ST001",
-            "name": "สถานีชาร์จรถยนต์ไฟฟ้า เซ็นทรัลเวิลด์",
-            "latitude": 13.746879,
-            "longitude": 100.539742,
-            "company": "PTT EV Station",
-            "status": {
-                "open_hours": "00:00",
-                "close_hours": "23:59",
-                "is_open": true
-            },
-            "connectors": [
-                {
-                    "connector_id": "CT0010",
-                    "type": "DC",
-                    "plug_name": "CHAdeMo",
-                    "price_per_unit": 6.5,
-                    "power_output": 150,
-                    "is_available": false
-                },
-                {
-                    "connector_id": "CT0011",
-                    "type": "AC",
-                    "plug_name": "Type 1",
-                    "price_per_unit": 4.5,
-                    "power_output": 22,
-                    "is_available": false
-                },
-                {
-                    "connector_id": "CT0012",
-                    "type": "DC",
-                    "plug_name": "CCS1",
-                    "price_per_unit": 6.8,
-                    "power_output": 100,
-                    "is_available": false
-                }
-            ]
-        }
         try {
             if (!API_BASE_URL) {
-                return station;
+                return mockStations.at(0);
             }
             const response = await fetch(`${API_BASE_URL}/station/${stationId}`);
             if (!response.ok) {
@@ -69,7 +31,7 @@ export const stationService = {
             return await response.json();
         } catch (error) {
             console.error('Error fetching stations:', error);
-            return station;
+            return mockStations.at(0);
         }
     }
 };
